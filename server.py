16bda169965c3a87621684b12a39c403f3e817d5
CanBot02 <guralcan@gmail.com>
2021-02-05 21:52:35 +0000

    alistairs snek solution

--- server.py
+++ server.py
@@ -3,69 +3,79 @@ import random
 
 import cherrypy
 
-"""
-This is a simple Battlesnake server written in Python.
-For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
-"""
-
 
 class Battlesnake(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        # This function is called when you register your Battlesnake on play.battlesnake.com
-        # It controls your Battlesnake appearance and author permissions.
-        # TIP: If you open your Battlesnake URL in browser you should see this data
-        return {
-            "apiversion": "1",
-            "author": "CanBot",  # TODO: Your Battlesnake Username
-            "color": "#FF00FF",  # TODO: Personalize
-            "head": "gamer",  # TODO: Personalize
-            "tail": "tiger-tail",  # TODO: Personalize
+      # This function is called when you register your Battlesnake on play.battlesnake.com
+      # It controls your Battlesnake appearance and author permissions.
+      # TIP: If you open your Battlesnake URL in browser you should see this data
+      return {
+        "apiversion": "1",
+        "author": "CanBot",  # TODO: Your Battlesnake Username
+        "color": "#FF00FF",  # TODO: Personalize
+        "head": "gamer",  # TODO: Personalize
+        "tail": "tiger-tail",  # TODO: Personalize
         }
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def start(self):
-        # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        data = cherrypy.request.json
+      # This function is called everytime your snake is entered into a game.
+      # cherrypy.request.json contains information about the game that's about to be played.
+      data = cherrypy.request.json
 
-        print("START")
-        return "ok"
+      print("START")
+      return "ok"
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
-        # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
-        data = cherrypy.request.json
-        snake = data['board']
-        print(snake)
+      # This function is called on every turn of a game. It's how your snake decides where to move.
+      # Valid moves are "up", "down", "left", or "right".
+      # TODO: Use the information in cherrypy.request.json to decide your next move.
+      data = cherrypy.request.json
+      snake = data['board']
+      print(snake)
 
-        {'game': {'id': '2b465ebb-dda6-49f6-b009-19160171c9e1', 'ruleset': {'name': 'solo', 'version': 'v1.0.17'}, 'timeout': 500},
-        'turn': 3,
-        'board':
-        {'height': 11, 'width': 11, 'snakes': [{'id': 'gs_Gk3W4pGpPDmmFyCFpbFpHFdS', 'name': 'FirstSnek2', 'latency': '57', 'health': 99, 'body': [{'x': 8, 'y': 3}, {'x': 8, 'y': 2}, {'x': 9, 'y': 2}, {'x': 9, 'y': 1}], 'head': {'x': 8, 'y': 3}, 'length': 4, 'shout': ''}],
-        'food': [{'x': 5, 'y': 5}],
-        'hazards': []},
-        'you': {'id': 'gs_Gk3W4pGpPDmmFyCFpbFpHFdS', 'name': 'FirstSnek2', 'latency': '57', 'health': 99,
-        'body': [{'x': 8, 'y': 3}, {'x': 8, 'y': 2}, {'x': 9, 'y': 2}, {'x': 9, 'y': 1}], 'head': {'x': 8, 'y': 3}, 'length': 4, 'shout': ''}}
+		  # List of positions of body
+		  body = data['you']['body']
+      # Position of head
+		  head = data['you']['head']
+      
+      possible_moves = ["up", "down", "left", "right"]
 
-        possible_moves = []
-        for pos in snake['you']['body']:
-            dif = snake['you']['head'] - pos
-            print(dif)
+      # Position of head if snake moves in direction
+		  up = head.copy(); up['y'] += 1
+		  down = head.copy(); down['y'] -= 1
+		  left = head.copy(); left['x'] -= 1
+		  right = head.copy(); right['x'] += 1
+       
+      print(head)
+      print(up)
+      print(down)
+      print(left)
+      print(right)
+
+      if (up in body) or (up['y'] >= """y limit"""):
+			  possible_moves.remove('up')
+	    if (down in body) or (down['y'] < 0):
+	  	  possible_moves.remove('down')
+	    if (left in body) or (left['x'] < 0):
+	  	  possible_moves.remove('left')
+	    if (right in body) or (right['x'] >= """x limit"""):
+	  	  possible_moves.remove('right')
         
+           
 
-        # Choose a random direction to move in
-        possible_moves = ["up", "down", "left", "right"]
-        move = random.choice(possible_moves)
+      # Choose a random direction to move in
+        
+      move = random.choice(possible_moves)
 
-        print(f"MOVE: {move}")
-        return {"move": move}
+      print(f"MOVE: {move}")
+      return {"move": move}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
