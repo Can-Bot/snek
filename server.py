3c8345bb20f2c6ccbf736ffd4e297690c2dbdc9f
CanBot02 <guralcan@gmail.com>
2021-02-06 09:45:06 +0000

    made nextPos into a dict

--- server.py
+++ server.py
@@ -19,9 +19,9 @@ class Battlesnake(object):
 		# TIP: If you open your Battlesnake URL in browser you should see this data
 		return {
 			"apiversion": "1",
-			"author": "Alistair Hewitt",  # TODO: Your Battlesnake Username
-			"color": "#ffa500",  # TODO: Personalize
-			"head": "default",  # TODO: Personalize
+			"author": "Alistair Hewitt, Djan Tanova",  # TODO: Your Battlesnake Username
+			"color": "#ff00ff",  # TODO: Personalize
+			"head": "gamer",  # TODO: Personalize
 			"tail": "default",  # TODO: Personalize
 		}
 
@@ -62,7 +62,7 @@ class Battlesnake(object):
 		leftNext = head.copy(); leftNext['x'] -= 1
 		rightNext = head.copy(); rightNext['x'] += 1
 
-		nextPos = [upNext, downNext, leftNext, rightNext]
+		nextPos = {"up":upNext, "down":downNext, "left":leftNext, "right":rightNext}
 		print(head); print(upNext); print(downNext); print(leftNext); print(rightNext)
 
 		# If move would result in collision with self, remove from possible moves
